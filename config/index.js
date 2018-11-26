c4f5733dccf848f193e0187762e207ba54ee7597
Luke <ReventonC@users.noreply.github.com>
2018-11-26 02:44:13 -0800

    modify config - 2

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: 'reventonc.github.io/DesignImpact/',
+    assetsPublicPath: 'DesignImpact/',
 
     /**
      * Source Maps
